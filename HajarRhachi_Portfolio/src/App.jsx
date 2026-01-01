de55038b2cb5370d41d2c9551481ecb17c0570b5
hajarrhachi <cc241010@fhstp.ac.at>
2026-01-01 20:42:06 +0100

    changes #4 to fix 404 mistake

--- HajarRhachi_Portfolio/src/App.jsx
+++ HajarRhachi_Portfolio/src/App.jsx
@@ -1,23 +1,18 @@
-import { useEffect } from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import CodingProjects from "./pages/CodingProjects";
 import DesignProjects from "./pages/DesignProjects";
 import About from "./pages/About";
-import Navbar from "./components/Navbar";
-
 
 export default function App() {
-    useEffect(() => {
-        if (!window.location.hash) {
-            window.location.hash = "#/";
-        }
-    }, []);
     return (
         <HashRouter>
             <Navbar />
             <Routes>
+                <Route path="*" element={<Navigate to="/" replace />} />
+
                 <Route path="/" element={<Home />} />
                 <Route path="/coding-projects" element={<CodingProjects />} />
                 <Route path="/design-projects" element={<DesignProjects />} />
